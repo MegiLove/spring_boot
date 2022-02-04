7b9a4a51d4214e2cfcdd01c7ee40d26cd5324970
unknown <ibisevic@naver.com>
2022-02-04 17:03:05 +0900

    02-04 mail보내기 기능구현

---
+++ mail_test/src/main/java/com/example/demo/SpringConfig.java
@@ -0,0 +1,31 @@
+package com.example.demo;
+
+import java.util.Properties;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+public class SpringConfig {
+	
+	@Bean
+	public JavaMailSenderImpl javaMailSender() {
+		JavaMailSenderImpl r= new JavaMailSenderImpl();
+		r.setHost("smtp.gmail.com");
+		r.setPort(587);
+		r.setUsername("shb04437");
+		r.setPassword("otpvfbhpfhzprqlq");
+		r.setDefaultEncoding("UTF-8");
+		
+		Properties prop= new Properties();
+		prop.put("mail.smtp.starttls.enable", true);
+		prop.put("mail.smtp.auth", true);
+		prop.put("mail.smtp.ssl.checkserveridentity", true);
+		prop.put("mail.smtp.ssl.trust", "*");
+		prop.put("mail.smtp.ssl.protocols", "TLSv1.2");
+		
+		r.setJavaMailProperties(prop);
+		
+		return r;
+	}
+}
