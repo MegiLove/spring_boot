6201657c39ae0f264fe0b705310b451f32c48abe
unknown <ibisevic@naver.com>
2022-02-14 18:55:27 +0900

    02-14 spring jquery ui

---
+++ jquery_ui/src/main/java/com/example/demo/controller/ScheduleController.java
@@ -0,0 +1,54 @@
+package com.example.demo.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.dao.ScheduleDAO;
+import com.example.demo.vo.ScheduleVO;
+
+import lombok.Setter;
+
+
+@RestController
+@Setter
+public class ScheduleController {
+	@Autowired
+	private ScheduleDAO dao;
+	
+	@RequestMapping("/insertSchedule")
+	public String insert(ScheduleVO s) {
+		int re  = dao.insert(s);
+		return re+"";
+	}
+	
+	@RequestMapping("/listSchedule")
+	public List<ScheduleVO> list(){
+		return dao.listSchedule();
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
